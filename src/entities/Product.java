34e2f794735e9746acec6601ecc8ae0d5f93612c
DouglasBasilio <basilio.douglas@yahoo.com.br>
2020-11-17 16:41:41 -0300

    add um construtor na classe Product

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -1,9 +1,16 @@
 package entities;
 
 public class Product {
+	
 	public String name;
 	public double price;
 	public int quantity;
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
 
 	public double totalValueInStock() {
 		return price * quantity;
